6255de35ea71b766f69069a27ddcfee2c7c8f867
Niek Neuij <niek.neuij@locamation.nl>
2019-09-16 17:37:01 +0200

    It's better to let the back button of visitor_out.php point to itself. Less annoying that way.

--- public/visitor_out.php
+++ public/visitor_out.php
@@ -88,6 +88,6 @@ else
 
 $dbconnection->close();
 ?>
-<div style="position:fixed;left:0px;bottom:0px;top:auto;right:auto;"><a href="." style="text-decoration:none;"><button style="font-size:24px;cursor:pointer;">⬅️ Back</button></a></div>
+<div style="position:fixed;left:0px;bottom:0px;top:auto;right:auto;"><a href="./visitor_out.php" style="text-decoration:none;"><button style="font-size:24px;cursor:pointer;">⬅️ Back</button></a></div>
 </body>
 </html>
