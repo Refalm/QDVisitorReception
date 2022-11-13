c3c7eb768ce80ae97ec05bf2c2919acd7a149db1
Niek Neuij <refalm1@gmail.com>
2022-11-13 13:44:39 +0100

    Employee type in reception page

--- public/reception/index.php
+++ public/reception/index.php
@@ -19,6 +19,7 @@ if ($whovisitors = $dbconnection->query("SELECT * FROM visitor ORDER BY arriveti
 {
 	if ($whovisitors->num_rows > 0)
 	{
+		echo "<form id=\"bezoeker\"><legend>Visitors</legend>";
 		echo "<table border=\"1\" cellpadding=\"10\">";
 		echo "<tr><th>Name</th><th>E-mail</th><th>Organisation</th><th>Host</th><th>Arrived</th><th>Departed</th><th></th></tr>";
 
@@ -35,12 +36,57 @@ if ($whovisitors = $dbconnection->query("SELECT * FROM visitor ORDER BY arriveti
 			echo "</tr>";
 		}
 		
-		echo "</table>";
+		echo "</table></form>";
 	}
 
 	else
 	{
 		echo "<span style=\"font-size:128px\">🗇</span><br /><br />The visitor list is empty...";
+	}
+
+	if ($whoemployees = $dbconnection->query("SELECT * FROM employee ORDER BY name"))
+	{
+		if ($whoemployees->num_rows > 0)
+		{
+			echo "<form id=\"werknemer\" method=\"post\" action=\".\">";
+			echo "<table border=\"1\" cellpadding=\"10\">";
+			echo "<tr><th>Name</th><th></th></tr>";
+	
+			while ($row = $whoemployees->fetch_object())
+			{
+				echo "<tr>";
+				echo "<td>" . $row->name . "</td>";
+				echo "<td><abbr title=\"Sack employee\" style=\"text-decoration:none\"><a href=\"delete.php?name=" . $row->name . "\" style=\"text-decoration:none\">❌</a></abbr></td>";
+				echo "</tr>";
+			}
+			
+			echo "</table>";
+			echo "<fieldset><legend>Employee</legend><label for=\"employeename\">Employee name</label><br /><input name=\"employeename\" type=\"text\" placeholder=\"Henk de Vries\" required class=\"wide\" /><br /><input name=\"submit\" type=\"submit\" value=\"🖋 New employee\" class=\"wide widesubmit\" /></fieldset></form>";
+		}
+	}
+	
+	else
+	{
+		echo "<span style=\"font-size:128px\">🗇</span><br /><br />The employee list is empty...";
+	}
+
+	if((isset($_POST['employeename'])))
+	{
+		$employeename = mysqli_real_escape_string($dbconnection, $_POST['employeename']);
+		
+		$checkemployeename = $_POST['employeename'];
+		$check = mysqli_query($dbconnection,"SELECT * FROM employee WHERE name = '".$checkemployeename."'");
+		
+		if (($insert = $dbconnection->prepare("INSERT INTO employee (name, present) VALUES ('".$employeename."',0)")) && (mysqli_num_rows($check)==0))
+		{
+			$insert->execute();
+			$insert->close();
+			echo "<meta http-equiv=\"refresh\" content=\"60; URL=.\" /></head><body id=\"success\"><span class=\"bigfont\">😺</span>\n<br /><br /><span class=\"tekst_header\">$employeename entered.</span>";
+		}
+		else
+		{
+			echo "<meta http-equiv=\"refresh\" content=\"60; URL=.\" /></head><body id=\"context\"><span class=\"bigfont\">😼</span>\n<br /><br /><span class=\"tekst_header\">$employeename is already on the list.</span>";
+		}
 	}
 }
 
