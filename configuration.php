525235f6f0747ca6e7d763921ae175ea51be236d
Niek Neuij <refalm1@gmail.com>
2022-11-13 12:33:39 +0100

    MariaDB wachtwoord hoef je nu maar één keer te doen

--- configuration.php
+++ configuration.php
@@ -1,14 +1,12 @@
 <?php
-# https://stackoverflow.com/questions/55756486/php-mysqli-create-database-if-does-not-exist
-
 require_once("vendor/autoload.php");
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$servername = $_ENV['MYSQL_HOST'];
-$username = $_ENV['MYSQL_USER'];
-$password = $_ENV['MYSQL_PASSWORD'];
-$database = $_ENV['MYSQL_DATABASE'];
+$servername = $_ENV['MARIADB_HOST'];
+$username = $_ENV['MARIADB_USER'];
+$password = $_ENV['MARIADB_PASSWORD'];
+$database = $_ENV['MARIADB_DATABASE'];
 $logo = $_ENV['LOGO'];
 $privacymail = $_ENV['PRIVACYMAIL'];
 
