bed5c762d02c6ae80cc8cb3e3bbaf39b79f4c877
Niek Neuij <niek.neuij@xip-onlineservices.nl>
2024-09-07 17:00:41 +0200

    teveel li's

--- public/taal/ie.php
+++ public/taal/ie.php
@@ -33,7 +33,7 @@ $taal['INPUT']					= 'Iontráil';
 $taal['NAME_EMPLOYEE4VISIT']	= 'Ainm an fhostaí a bhfuil tú ag tabhairt cuairte air';
 
 $taal['VISITORIN_TITLE']		= 'Clárú cuairteoirí';
-$taal['VISITORIN_HANDLING']		= 'Próiseálaimid na sonraí thíos chun calraí a láimhseáil i gceart agus stórálfaimid na sonraí seo ar feadh dhá lá ar a mhéad:<ul><li>Céadainm agus sloinne;</li><li>Eagraíocht a bhfuil baint agat léi;</li><li><li>Dáta agus am teachta agus imeachta;</li><li>Eolas a bhaineann leis an duine nó leis an roinn ar mian leat cuairt a thabhairt air.</li></ul><strong>Tá sé de cheart agat cur i gcoinne próiseáil na sonraí, agus chun scriosadh nó bac a chur ar do shonraí a iarraidh trí ríomhphost a sheoladh chuig<br />' .$privacymail. '</strong>';
+$taal['VISITORIN_HANDLING']		= 'Próiseálaimid na sonraí thíos chun calraí a láimhseáil i gceart agus stórálfaimid na sonraí seo ar feadh dhá lá ar a mhéad:<ul><li>Céadainm agus sloinne;</li><li>Eagraíocht a bhfuil baint agat léi;</li><li>Dáta agus am teachta agus imeachta;</li><li>Eolas a bhaineann leis an duine nó leis an roinn ar mian leat cuairt a thabhairt air.</li></ul><strong>Tá sé de cheart agat cur i gcoinne próiseáil na sonraí, agus chun scriosadh nó bac a chur ar do shonraí a iarraidh trí ríomhphost a sheoladh chuig<br />' .$privacymail. '</strong>';
 $taal['VISITORIN_RULESTITLE']	= 'Rialacha do chuairteoirí';
 $taal['VISITORIN_RULESCAM']		= 'Mura bhfuil cead scríofa agat ón mbainistíocht; gan grianghrafadóireacht, scannánú, agus/nó taifeadtaí fuaime a chruthú.';
 $taal['VISITORIN_RULESESD']		= 'Agus tú ag dul isteach i gcrios ESD, ní mór duit na treoracha a thugann an fhoireann a leanúint.';
